9ee714e38eed68d03eaa55a24e24af690130509f
samudraon <samudraon@gmailcom>
2023-03-20 23:00:00 +0600

    daynamic component added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 /* Variable */
 const num = 5555;
-const singer = { name: 'Dr. Mahfuz', job: 'Singer' };
-const singer2 = {
-  name: 'Eva Rahman',
-  job: 'Singer2'
-}
+const singers = [
+  { name: 'Dr. Mahfuz', job: 'Singer' },
+  { name: 'Eva Rahman', job: 'Singer2' },
+  { name: 'Agun', job: 'Sopno' },
+  { name: 'Shuvro', job: 'Pathor' }
+]
 
 /* CSS Style */
 const singerStyle = {
@@ -16,33 +17,45 @@ const singerStyle = {
 }
 
 function App() {
+  const nayoks = ['Rubel', 'Bapparaj', 'Kuber', 'Jashim', 'Salman Shah', 'Riyaj', 'Razzak']
   return (
     <div className="App">
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
+      {
+        nayoks.map(nayok => <li>Name: {nayok}</li>)
+      }
+
+      {/* {
+        nayoks.map(nayok => <Person name={nayok}></Person>)
+      } */}
+
+      {
+        singers.map(singer => <Person name={singer.name}></Person>)
+      }
+
+      {/* <Person name={nayoks[0]} nayika="Moushumi"></Person>
+      <Person name={nayoks[1]} nayika="Moushumi"></Person>
+      <Person name={nayoks[2]} nayika="kopila"></Person> */}
       <h5>New Component. YAY!!</h5>
-      <Friend></Friend>
-      <Friend></Friend>
-      <Friend></Friend>
+      <Friend movie="Daabang" phone="01234"></Friend>
+      <Friend movie="Kaliya" phone="019912"></Friend>
     </div>
   );
 }
 
-function Person() {
+function Person(props) {
   return (
     <div className='person'>
-      <h1>Shakib Al Hasan</h1>
-      <p>Profession: Cricketer</p>
+      <h1>{props.name}</h1>
+      <p>{props.nayika}</p>
     </div>
   )
 }
 
-function Friend() {
+function Friend(props) {
   return (
     <div className='container'>
-      <h3>Name: Salman Khan</h3>
-      <p>job: maramari</p>
+      <h3>Name: {props.movie}</h3>
+      <p>phone: {props.phone}</p>
     </div>
   )
 }
